8e5a800fad983694b00ac0bf1a346033d538f4e9
Lineu Pastorelli <lineualberto@gmail.com>
2020-03-23 12:45:38 -0300

    add dependencias do projeto

--- backend/src/controllers/DevControllers.js
+++ backend/src/controllers/DevControllers.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
 const parseStringAsArray = require('../utils/parseStringAsArray');
+const { findConnections, sendMessage } = require('../websocket');
 
 module.exports = {
   async index(request, response) {
@@ -8,23 +9,23 @@ module.exports = {
 
     return response.json(devs);
   },
-
+  
   async store(request, response) {
     const { github_username, techs, latitude, longitude } = request.body;
 
     let dev = await Dev.findOne({ github_username });
 
-    if(!dev) {
-      const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
+    if (!dev) {
+      const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
   
       const { name = login, avatar_url, bio } = apiResponse.data;
-
+    
       const techsArray = parseStringAsArray(techs);
     
-      const location= {
+      const location = {
         type: 'Point',
         coordinates: [longitude, latitude],
-      }
+      };
     
       dev = await Dev.create({
         github_username,
@@ -34,8 +35,15 @@ module.exports = {
         techs: techsArray,
         location,
       })
+
+      const sendSocketMessageTo = findConnections(
+        { latitude, longitude },
+        techsArray,
+      )
+
+      sendMessage(sendSocketMessageTo, 'new-dev', dev);
     }
-    
+  
     return response.json(dev);
-  }
-}
+  },
+};
